fefbc0766efc88bb2de496cbfe35a2063ea752e0
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2020-03-23 20:48:28 -0300

    editar e deletar cliente

--- fetchApi/listagem-clientes.js
+++ fetchApi/listagem-clientes.js
@@ -20,21 +20,28 @@ const corpoTabela = document.querySelector("[data-conteudo-tabela]")
 
 corpoTabela.innerHTML = conteudoLinha*/
 
+const removeCliente = id => {
+  if(confirm("Deseja Deletar o cliente?")) {
+    deletaCliente(id)
+  }
+}
 
 const corpoTabela = document.querySelector("[data-conteudo-tabela]")
 
 listarClientes().then( exibe => {
   exibe.forEach(indice => {
-    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome))
+    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
   });
 });
 
-const exibeCliente = (cpf, nome) => {
+const exibeCliente = (cpf, nome, id) => {
   const linha = document.createElement('tr')
 
   const conteudoLinha = `
     <td>${cpf}</td>
     <td>${nome}</td>
+    <button type="button" class="btn btn-danger" onclick="removeCliente(${id})">Excluir</button>
+  <a href="edita-clientes.html?id=${id}"><button type="button" class ="btn btn-info">Editar</button></a>
   `
 
   linha.innerHTML = conteudoLinha;
